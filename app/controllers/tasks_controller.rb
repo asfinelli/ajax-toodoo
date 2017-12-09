58720756f5a34199d0551e2d01b7d78a29839402
Drew Finelli <asfinelli@gmail.com>
2017-12-09 14:47:03 -0500

    first update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,8 @@ class TasksController < ApplicationController
   # GET /tasks.json
   def index
     @tasks = Task.all
+    @tasks = Task.new
+    sleep 1
   end
 
   # GET /tasks/1
@@ -28,7 +30,8 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        #format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to tasks_url }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
